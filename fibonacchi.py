32103d5f9e9fb051fe9513a47f23e118c213ea6c
hzgamer66 <64765794+hzgamer66@users.noreply.github.com>
2020-05-15 15:27:19 -0700

    Update fibonacchi.py

--- fibonacchi.py
+++ fibonacchi.py
@@ -1,9 +1,7 @@
-print("How many numbers of the fibonacci sequence would you like to print?")
-num = input()
-num = int(num)
+num = int(input('How many numbers of the fibonacci sequence would you like to print? '))
 sequence = [0, 1]
 
-for x in range(num-2):
-    sequence.append(sequence[x]+sequence[x+1])
+for x in range(num - 2):
+    sequence.append(sequence[x] + sequence[x + 1])
 
 print(*sequence, sep=", ")
